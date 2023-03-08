33a3935cf9f2ad4db4fb72f64e6ea3c12e21472d
d4daveshep <david@daveshep.net.nz>
2023-03-08 13:46:48 +1300

    fixed fixture by moving data add to separate function

--- test_functional.py
+++ test_functional.py
@@ -9,24 +9,6 @@ from graph import Graph
 def graph():
     g = Graph("test")
 
-    g.add_connection("Andrew", "is a", "Permanent Employee")
-    g.add_connection("Andrew", "has title", "Chief Engineer")
-    g.add_connection("Andrew", "worked on", "TWG")
-    g.add_connection("Andrew", "worked on", "GDT")
-    g.add_connection("Andrew", "knows", "Java")
-    g.add_connection("Andrew", "knows", "Spring Boot")
-
-    g.add_connection("Chris", "is a", "Contractor")
-    g.add_connection("Chris", "has title", "Senior Software Engineer")
-    g.add_connection("Chris", "works on", "GDT")
-    g.add_connection("Chris", "knows", "Java")
-    g.add_connection("Chris", "knows", "Mulesoft")
-
-    g.add_connection("Paul", "is a", "Permanent Employee")
-    g.add_connection("Chris", "has title", "Senior SRE")
-    g.add_connection("Chris", "works on", "GDT")
-    g.add_connection("Chris", "knows", "Java")
-    g.add_connection("Chris", "knows", "Mulesoft")
 
     # counts should be: 16 connections, 13 nodes
 
@@ -36,7 +18,31 @@ def graph():
     # os.remove("test_nodes.json")
     os.remove("test_connections.json")
 
+def set_up_data(graph:Graph):
+    graph.add_connection("Andrew", "is a", "Permanent Employee")
+    graph.add_connection("Andrew", "has title", "Chief Engineer")
+    graph.add_connection("Andrew", "worked on", "TWG")
+    graph.add_connection("Andrew", "worked on", "GDT")
+    graph.add_connection("Andrew", "knows", "Java")
+    graph.add_connection("Andrew", "knows", "Spring Boot")
+
+
+    graph.add_connection("Chris", "is a", "Contractor")
+    graph.add_connection("Chris", "has title", "Senior Software Engineer")
+    graph.add_connection("Chris", "works on", "GDT")
+    graph.add_connection("Chris", "knows", "Java")
+    graph.add_connection("Chris", "knows", "Mulesoft")
+
+    graph.add_connection("Paul", "is a", "Permanent Employee")
+    graph.add_connection("Chris", "has title", "Senior SRE")
+    graph.add_connection("Chris", "works on", "GDT")
+    graph.add_connection("Chris", "knows", "Java")
+    graph.add_connection("Chris", "knows", "Mulesoft")
+
+
 def test_full_CRUD_use_case(graph):
+
+    set_up_data(graph)
 
     # check graph size
     assert graph.count_connections() == 16
